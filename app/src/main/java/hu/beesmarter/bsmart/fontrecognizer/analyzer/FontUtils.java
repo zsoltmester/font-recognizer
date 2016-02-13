cfe1871a4ec0066c2a2a7a93a853c68b734155bf
alit <ali.tams@gmail.com>
2016-02-13 16:09:45 +0100

    Added font names.

--- app/src/main/java/hu/beesmarter/bsmart/fontrecognizer/analyzer/FontUtils.java
+++ app/src/main/java/hu/beesmarter/bsmart/fontrecognizer/analyzer/FontUtils.java
@@ -14,6 +14,31 @@ public class FontUtils {
 
 	static {
 		fonts.add(new Font("Akiza Sans"));
-		// TODO add the other fonts
+		fonts.add(new Font("Autonym"));
+		fonts.add(new Font("Averia Serif"));
+		fonts.add(new Font("Crimson"));
+		fonts.add(new Font("Deja Vu Serif"));
+		fonts.add(new Font("Fanwood"));
+		fonts.add(new Font("GFS Artemisia"));
+		fonts.add(new Font("Khmer OS Classic"));
+		fonts.add(new Font("Liberation Serif"));
+		fonts.add(new Font("Libre Bodoni"));
+		fonts.add(new Font("Quattrocento"));
+		fonts.add(new Font("Akiza Sans"));
+		fonts.add(new Font("Averia Sans"));
+		fonts.add(new Font("Coval"));
+		fonts.add(new Font("Comic Relief"));
+		fonts.add(new Font("Cursive Sans"));
+		fonts.add(new Font("Cursive Serif"));
+		fonts.add(new Font("Deja Vu Sans"));
+		fonts.add(new Font("Fibel Nord"));
+		fonts.add(new Font("Free Universal"));
+		fonts.add(new Font("Liberation Sans"));
+		fonts.add(new Font("Segoe UI Symbol"));
+		fonts.add(new Font("Anonymus Pro"));
+		fonts.add(new Font("Courier Code"));
+		fonts.add(new Font("Dancing Script"));
+		fonts.add(new Font("Katamotz Ikasi"));
+		fonts.add(new Font("Petit Formal Script"));
 	}
 }
